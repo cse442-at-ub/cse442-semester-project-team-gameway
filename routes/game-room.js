71c1dc9937b5ed858c13b819abe30705910d8674
StevenLiu <sliu57@buffalo.edu>
2020-04-16 12:52:42 -0400

    Updated pages.js to go to correct /rank route Closed bracket for game-room.js on line 72

--- routes/game-room.js
+++ routes/game-room.js
@@ -22,47 +22,53 @@ let roomID = "";
 
             let user = req.session.user;
 
-        if(user) {
-            console.log(user);
+            if (user) {
+                console.log(user);
 
-            let sql = "SELECT * FROM Friendship WHERE UserID1 = ? OR UserID2 = ?";
+                let sql = "SELECT * FROM Friendship WHERE UserID1 = ? OR UserID2 = ?";
 
-            pool.query(dbName, (err, result) => {
-                if (err) throw err;
-            });
+                pool.query(dbName, (err, result) => {
+                    if (err) throw err;
+                });
 
-            pool.query(sql, [user['ID'], user['ID']], (err, relationships) => {
-                let friendIDs = [];
-                for (let x = 0; x < relationships.length; x++) {
-                    if (relationships[x]['UserID1'] !== user['ID'] && friendIDs.includes(relationships[x]['UserID1']) === false) {
-                        friendIDs.push(relationships[x]['UserID1'])
-                    } else if (relationships[x]['UserID2'] !== user['ID'] && friendIDs.includes(relationships[x]['UserID2']) === false) {
-                        friendIDs.push(relationships[x]['UserID2'])
-                    }
-                }
-
-                if (friendIDs.length > 0) {
-                    let sql = "SELECT * FROM User WHERE ";
-                    for (let x = 0; x < friendIDs.length; x++) {
-                        sql = sql.concat("ID = ", friendIDs[x]);
-                        if (x + 1 !== friendIDs.length) {
-                            sql = sql.concat(" OR ");
+                pool.query(sql, [user['ID'], user['ID']], (err, relationships) => {
+                    let friendIDs = [];
+                    for (let x = 0; x < relationships.length; x++) {
+                        if (relationships[x]['UserID1'] !== user['ID'] && friendIDs.includes(relationships[x]['UserID1']) === false) {
+                            friendIDs.push(relationships[x]['UserID1'])
+                        } else if (relationships[x]['UserID2'] !== user['ID'] && friendIDs.includes(relationships[x]['UserID2']) === false) {
+                            friendIDs.push(relationships[x]['UserID2'])
                         }
                     }
 
-                    pool.query(sql, (err, friends) => {
-                        console.log(friends);
-                        res.render('room-list', {results: results, opp: req.session.opp, user: user, friends: friends});
+                    if (friendIDs.length > 0) {
+                        let sql = "SELECT * FROM User WHERE ";
+                        for (let x = 0; x < friendIDs.length; x++) {
+                            sql = sql.concat("ID = ", friendIDs[x]);
+                            if (x + 1 !== friendIDs.length) {
+                                sql = sql.concat(" OR ");
+                            }
+                        }
+
+                        pool.query(sql, (err, friends) => {
+                            console.log(friends);
+                            res.render('room-list', {
+                                results: results,
+                                opp: req.session.opp,
+                                user: user,
+                                friends: friends
+                            });
+                            return;
+                        });
+                    } else {
+                        res.render('room-list', {results: results, opp: req.session.opp, user: user, friends: []});
                         return;
-                    });
-                } else {
-                    res.render('room-list', {results: results, opp: req.session.opp, user: user, friends: []});
-                    return;
-                }
-            });
-            return;
-        }
-        res.redirect('/');
+                    }
+                });
+                return;
+            }
+            res.redirect('/');
+        });
     });
 
 <!--Insert New Game Room-->
